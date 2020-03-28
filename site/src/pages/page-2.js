b6ab29e1713b1ee8a46075a76a103c64a958eba1
frransier <frransier@gmail.com>
2020-03-28 20:39:28 +0100

    test

--- site/src/pages/page-2.js
+++ site/src/pages/page-2.js
@@ -1,16 +1,113 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import CanvasJSReact from "../data/canvasjs.react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import data from "../data/data"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  var CanvasJS = CanvasJSReact.CanvasJS
+  var CanvasJSChart = CanvasJSReact.CanvasJSChart
+  console.log(data)
+
+  const options = {
+    animationEnabled: true,
+    theme: "light2",
+    logarithmBase: 4,
+    title: {
+      text: "Day 1 = 5 deaths",
+    },
+    axisY: {
+      title: "Deaths",
+      logarithmic: true,
+      includeZero: false,
+    },
+    axisX: {
+      crosshair: {
+        enabled: true,
+        labelFormatter: e => {
+          return "Days since 5 deaths"
+        },
+      },
+      gridThickness: 0,
+      tickLength: 0,
+      lineThickness: 0,
+      labelFontColor: "transparent",
+    },
+    data: [
+      {
+        type: "spline",
+        name: "Sweden",
+        showInLegend: true,
+        dataPoints: data.data.find(x => x.country === "Sweden").data,
+      },
+      {
+        type: "spline",
+        name: "Denmark",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "Denmark")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Norway",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "Norway")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Spain",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "Spain")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "South Korea",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "South_Korea")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "USA",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "United_States_of_America")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Italy",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "Italy")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "France",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "France")
+          .data.sort((a, b) => b.label > a.label),
+      },
+    ],
+  }
+  console.log(options.data)
+
+  return (
+    <Layout>
+      <SEO title="Page two" />
+
+      <CanvasJSChart options={options} />
+    </Layout>
+  )
+}
 
 export default SecondPage
---
+++ studio/canvas.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("covidtracker0328.json"));
+const iso = JSON.parse(fs.readFileSync("iso.json"));
+const _ = require("lodash");
+
+const test = data
+  .map((x, i) => {
+    if (
+      i > 0 &&
+      x.countriesAndTerritories !== data[i - 1].countriesAndTerritories
+    ) {
+      // if (x.deaths > 9)
+      // { y: 155, label: "Jan" },
+      const country = data
+        .map(y => {
+          if (y.geoId === x.geoId) return y;
+        })
+        .filter(Boolean);
+      // console.log(grouped);
+      // const cnt = grouped[0];
+
+      const spine = country
+        .map((x, i) => {
+          const arr = country.slice(i).map(x => x.deaths);
+
+          const point = {
+            y: arr.reduce((a, b) => a + b, 0),
+            label: i + 1,
+          };
+          if (point.y > 4) return point;
+        })
+        .filter(Boolean);
+
+      //   console.log(spine.length);
+
+      const reverse = spine.map((x, i) => {
+        const pt = {
+          y: x.y,
+          label: `${country[0].countriesAndTerritories} day ${
+            spine[spine.length - i - 1].label
+          } `,
+          x: spine[spine.length - i - 1].label,
+        };
+        return pt;
+      });
+      //   console.log(reverse);
+
+      const final = {
+        country: country[0].countriesAndTerritories,
+        data: reverse,
+      };
+
+      return final;
+    }
+
+    // const deaths = grouped.map(x => x.deaths).reduce((a, b) => a + b, 0);
+    // const cases = grouped.map(x => x.cases).reduce((a, b) => a + b, 0);
+    // const name = grouped[0]["countriesAndTerritories"].replace("_", " ");
+    // const geo = iso.find(x => x.ISO === grouped[0].geoId);
+
+    // const country = {
+    //   _type: "country",
+    //   _id: geo && `1${Math.round(geo.Latitude)}${Math.round(geo.Longitude)}`,
+    //   name: name,
+    //   deaths: deaths,
+    //   newDeaths: deaths - (deaths - grouped[0].deaths),
+    //   cases: cases,
+    //   newCases: cases - (cases - grouped[0].cases),
+    //   lat: geo && geo.Latitude,
+    //   lng: geo && geo.Longitude,
+    // };
+    // if (geo && country.cases > 20) return country;
+  })
+
+  .filter(Boolean);
+// console.log(test);
+
+// const unique = _.uniqBy(test, "country");
+// console.log(unique);
+
+// console.log(test);
+
+// console.log(test.find(x => x.name === "United Kingdom"));
+
+fs.writeFileSync("logScale.json", JSON.stringify(test));
+// cat data.json | jq -c '.[]' > data.ndjson
