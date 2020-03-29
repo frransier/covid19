8b896dc95661d60edc6a7f794f440915d7235450
frransier <frransier@gmail.com>
2020-03-29 10:52:07 +0200

    per capita

--- site/src/data/chart.js
+++ site/src/data/chart.js
@@ -114,8 +114,118 @@ const Chart = () => {
       },
     ],
   }
+  const options2 = {
+    animationEnabled: true,
+    theme: "light2",
+    logarithmBase: 4,
+    title: {
+      text: "Day 1 = 5 deaths",
+    },
+    axisY: {
+      title: "Deaths per capita",
+      logarithmic: true,
+      includeZero: false,
+    },
+    axisX: {
+      crosshair: {
+        enabled: true,
+        labelFormatter: e => {
+          return "Days since 5 deaths"
+        },
+      },
+      gridThickness: 0,
+      tickLength: 0,
+      lineThickness: 0,
+      labelFontColor: "transparent",
+    },
+    data: [
+      {
+        type: "spline",
+        name: "Sweden",
+        showInLegend: true,
+        dataPoints: data.perCapita.find(x => x.country === "Sweden").data,
+      },
+      {
+        type: "spline",
+        name: "Denmark",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "Denmark")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Norway",
+        showInLegend: true,
+        dataPoints: data.data
+          .find(x => x.country === "Norway")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Spain",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "Spain")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Germany",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "Germany")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Netherlands",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "Netherlands")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Belgium",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "Belgium")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "Italy",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "Italy")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "United Kingdom",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "United_Kingdom")
+          .data.sort((a, b) => b.label > a.label),
+      },
+      {
+        type: "spline",
+        name: "France",
+        showInLegend: true,
+        dataPoints: data.perCapita
+          .find(x => x.country === "France")
+          .data.sort((a, b) => b.label > a.label),
+      },
+    ],
+  }
 
-  return <CanvasJSChart options={options} />
+  return (
+    <div>
+      <CanvasJSChart options={options} />
+      <CanvasJSChart options={options2} />
+    </div>
+  )
 }
 
 export default Chart
--- studio/canvas.js
+++ studio/canvas.js
@@ -24,10 +24,10 @@ const test = data
           const arr = country.slice(i).map(x => x.deaths);
 
           const point = {
-            y: arr.reduce((a, b) => a + b, 0),
+            y: arr.reduce((a, b) => a + b, 0) / country[0].popData2018,
             label: i + 1,
           };
-          if (point.y > 4) return point;
+          if (point.y * country[0].popData2018 > 4) return point;
         })
         .filter(Boolean);
 
